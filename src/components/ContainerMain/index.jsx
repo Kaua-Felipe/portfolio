22bfcc4630efc69cd2352c6267636e12639f99b6
Kauã <kauafelipeoficial191@gmail.com>
2022-08-17 13:30:58 -0300

    Comitando alteração do src da imagem

--- src/components/ContainerMain/index.jsx
+++ src/components/ContainerMain/index.jsx
@@ -9,7 +9,8 @@ import { Container, ContainerContent } from "./styles"
 export default function ContainerMain() {
     return (
         <Container>
-            <img src="../../../assets/student.png" alt="Imagem de Cartoon de estudante" />
+            {/* <img src="../../../assets/student.png" alt="Imagem de Cartoon de estudante" /> */}
+            <img src="https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/ARTIGO-ESTUDOS.png" alt="Imagem de Cartoon de estudante" />
             <NavBar />
             <ContainerContent>
                 <About />
