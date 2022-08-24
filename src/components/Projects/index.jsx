afefad1eb7256d195a4b5152f05dcf2ac860f18f
Kauã <kauafelipeoficial191@gmail.com>
2022-08-23 21:17:52 -0300

    Baixando e adicionando imagens de cada projeto correspondente

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Project from "../Project"
 import { ContainerProjects, AllProjects } from "./styles"
+import cloneTwitter from "../../../assets/imgs-projects/clone-twitter.jpg"
+import JqueryAjaxAula from "../../../assets/imgs-projects/jquery-ajax-aula.jpg"
+import Portfolio from "../../../assets/imgs-projects/portfolio.jpg"
+import AnimalsHope from "../../../assets/imgs-projects/animals-hope.jpg"
+import ProjetoEletiva from "../../../assets/imgs-projects/projeto-eletiva.jpg"
+import GithubAPIInterface from "../../../assets/imgs-projects/github-api-interface.jpg"
 
 export default function Projects() {
     const [reposData, setReposData] = useState()
     const [listImgs] = useState([
-        "https://images.pexels.com/photos/3585092/pexels-photo-3585092.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
-        "https://images.pexels.com/photos/1148399/pexels-photo-1148399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
-        "https://images.pexels.com/photos/6958542/pexels-photo-6958542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
-        "https://images.pexels.com/photos/730706/pexels-photo-730706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
-        "https://images.pexels.com/photos/970080/pexels-photo-970080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
-        "https://images.pexels.com/photos/220938/pexels-photo-220938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+        /*"https://images.pexels.com/photos/3585092/pexels-photo-3585092.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+		cloneTwitter, 
+        /*"https://images.pexels.com/photos/1148399/pexels-photo-1148399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+		GithubAPIInterface, 
+        /*"https://images.pexels.com/photos/6958542/pexels-photo-6958542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+		JqueryAjaxAula, 
+        /*"https://images.pexels.com/photos/730706/pexels-photo-730706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+		Portfolio, 
+        /*"https://images.pexels.com/photos/970080/pexels-photo-970080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+		AnimalsHope, 
+        /*"https://images.pexels.com/photos/220938/pexels-photo-220938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+		ProjetoEletiva, 
         "https://images.pexels.com/photos/5302811/pexels-photo-5302811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     ])
     useEffect(() => {
