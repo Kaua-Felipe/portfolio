e53ccee93b738b07d999aaab82d342c67f2087d1
Kauã Felipe Alves <kauafelipeoficial191@gmail.com>
2024-03-09 18:03:23 -0300

    Resolvendo bug de não mostrar imagem de projeto

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,7 +6,7 @@ export default function About() {
         <ContainerAbout id="about">
             <h2>Sobre</h2>
             <p data-animation="bottom">
-                Olá, meu nome é Kauã Felipe Alves, tenho 18 anos e nasci na cidade de Araçatuba-SP. Atualmente, moro com meu irmão e minha mãe, mas busco a minha independência financeira. Para conseguir isso, busco um espaço no mercado de trabalho na área da tecnologia, especificamente na área do desenvolvimento web, área pela qual senti interesse quando fiz o curso de Desenvolvimento de Sistemas. A minha curiosidade me levou a criar o meu próprio site aos 17 anos.
+                Olá, meu nome é Kauã Felipe Alves, tenho 19 anos e nasci na cidade de Araçatuba-SP. Atualmente, moro com meu irmão e minha mãe, mas busco a minha independência financeira. Para conseguir isso, busco um espaço no mercado de trabalho na área da tecnologia, especificamente na área do desenvolvimento web, área pela qual senti interesse quando fiz o curso de Desenvolvimento de Sistemas e cursanso Ciências da Computação. A minha curiosidade me levou a criar o meu site aos 17 anos, e futuramente construir projetos maiores.
             </p>
         </ContainerAbout>
     )
